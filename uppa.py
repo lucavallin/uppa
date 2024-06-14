22ca4d3329c0a1be79df673d7c8a61a6deb36326
Luca Cavallin <lucavallin@users.noreply.github.com>
2024-06-14 22:13:56 +0200

    fix linting problems

--- uppa.py
+++ uppa.py
@@ -1,17 +1,8 @@
-# What does this script do?
-#
-# 1. Fetches photos from Unsplash
-# 2. Calculates a score for each photo: It calculates the raw performance scores based on views per day and downloads per view.
-# 3. Identifies "Upper Bound Outliers": it uses the IQR method to calculate the upper bound. Only photos with raw scores above this upper bound are considered outliers.
-# 4. Filters "Upper Bound Outliers": it filters out the photos identified as upper bound outliers.
-# 5. Recalculates Min and Max scores: The minimum and maximum raw scores are recalculated after removing the upper bound outliers.
-# 6.Normalizing Scores: It normalizes the scores of the remaining photos to a scale of 0 to 100.
-# 7.Output: The script prints the normalized scores and optionally saves them to a JSON file.
-
+"""Fetch photos from Unsplash API, calculate performance scores, display the results."""
 import os
+import json
 import requests
 import numpy as np
-import json
 import matplotlib.pyplot as plt
 
 # Read ACCESS_KEY and USERNAME from environment variables
@@ -25,8 +16,9 @@ if not ACCESS_KEY or not USERNAME:
 
 PHOTOS_FILENAME = 'photos.json'
 
-# Function to get all photos from Unsplash API with pagination
+
 def get_unsplash_photos(username, access_key):
+    """Function to get all photos from Unsplash API with pagination"""
     url = f"https://api.unsplash.com/users/{username}/photos"
     all_photos = []
     page = 1
@@ -53,15 +45,18 @@ def get_unsplash_photos(username, access_key):
 
     return all_photos
 
-# Function to save photos to a file
+
 def save_photos_to_file(photos, filename):
-    with open(filename, 'w') as f:
+    """Function to save photos to a file"""
+    with open(filename, 'w', encoding='utf-8') as f:
         json.dump(photos, f, indent=4)
 
-# Function to load photos from a file
+
 def load_photos_from_file(filename):
-    with open(filename, 'r') as f:
+    """Function to load photos from a file"""
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
+
 
 # Load or fetch photos
 if os.path.exists(PHOTOS_FILENAME):
@@ -76,16 +71,18 @@ else:
     print("Fetched photos from Unsplash API and saved to photos.json")
 
 # Calculate raw performance scores and filter photos
-W_v = 1.5
-W_d = 0.5
+W_V = 1.5
+W_D = 0.5
 filtered_photos = []
 
 for photo in photos:
-    days_online = (np.datetime64('now') - np.datetime64(photo['created_at'])).astype(int) / (24 * 60 * 60)
+    days_online = (np.datetime64(
+        'now') - np.datetime64(photo['created_at'])).astype(int) / (24 * 60 * 60)
     if days_online >= 30:
-        views = photo['statistics']['views']['total'] or 1  # Avoid division by zero
+        # Avoid division by zero
+        views = photo['statistics']['views']['total'] or 1
         downloads = photo['statistics']['downloads']['total']
-        raw_score = (views / days_online) * W_v + (downloads / views) * W_d
+        raw_score = (views / days_online) * W_V + (downloads / views) * W_D
         photo['days_online'] = days_online
         photo['raw_score'] = raw_score
         filtered_photos.append(photo)
@@ -98,22 +95,32 @@ IQR = Q3 - Q1
 upper_bound = Q3 + 1.5 * IQR
 
 # Filter out upper bound outliers
-filtered_photos = [photo for photo in filtered_photos if photo['raw_score'] <= upper_bound]
+filtered_photos = [
+    photo for photo in filtered_photos if photo['raw_score'] <= upper_bound]
 
 # Recalculate min and max raw scores after filtering
-filtered_raw_scores = np.array([photo['raw_score'] for photo in filtered_photos])
+filtered_raw_scores = np.array([photo['raw_score']
+                               for photo in filtered_photos])
 min_raw = filtered_raw_scores.min()
 max_raw = filtered_raw_scores.max()
 
 # Normalize scores
 for photo in filtered_photos:
-    photo['normalized_score'] = ((photo['raw_score'] - min_raw) / (max_raw - min_raw)) * 100
+    photo['normalized_score'] = (
+        (photo['raw_score'] - min_raw) / (max_raw - min_raw)) * 100
     photo['days_online'] = int(photo['days_online'])
 
 # Sort photos by normalized score and display the results
-sorted_photos = sorted(filtered_photos, key=lambda x: x['normalized_score'], reverse=True)
+sorted_photos = sorted(
+    filtered_photos, key=lambda x: x['normalized_score'], reverse=True)
 for photo in sorted_photos:
-    print(f"Photo URL: https://unsplash.com/photos/{photo['id']}, Days Online: {photo['days_online']}, Views: {photo['statistics']['views']['total']}, Downloads: {photo['statistics']['downloads']['total']}, Normalized Score: {photo['normalized_score']:.2f}")
+    photo_id = photo['id']
+    days_online = photo['days_online']
+    views = photo['statistics']['views']['total']
+    downloads = photo['statistics']['downloads']['total']
+    normalized_score = photo['normalized_score']
+    print(f"Photo URL: https://unsplash.com/photos/{photo_id}, Days Online: {days_online}, Views: {
+          views}, Downloads: {downloads}, Normalized Score: {normalized_score:.2f}")
 
 # Generate a bar chart
 photo_ids = [photo['id'] for photo in sorted_photos]
